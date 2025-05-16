c7cd1ec929c90f446c350eef98bf6bcc35fa7aaa
DESKTOP-9L1QH26\MSI <isurugayashan179@gmail.com>
2025-05-16 16:00:09 +0530

    update datepicker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -15,27 +15,23 @@ import { FormControl } from "./ui/form"
 export function DatePicker({ field, className }) {
   return (
     <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-full justify-start text-left font-normal",
-            !field.value && "text-muted-foreground",
-            className
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0 z-50" align="start">
-        <Calendar
-          mode="single"
-          selected={field.value ?? undefined}
-          onSelect={field.onChange}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button variant="outline" className="w-full pl-3 text-left font-normal">
+                        {field.value ? format(field.value, "PPP") : <span>Select check-in date</span>}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) => date < new Date(new Date().setHours(0, 0, 0, 0))}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
   );
 }
