bf96bf277d8ab00c11b227a526499d5cc7319b0f
DESKTOP-9L1QH26\MSI <isurugayashan179@gmail.com>
2025-05-16 22:50:19 +0530

    clean up

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -9,7 +9,7 @@ function HomePage(){
         <div className="relative min-h-screen">
         <Hero/>
         {/* <img src="/assets/hero/hero_1.jpg" alt="" className="absolute top-0 left-0 w-full h-full object-cover -z-10  "/> */}
-        <div className=" bg-cover bg-center animate-zoomIn absolute top-0 left-0 w-full h-full object-cover -z-10" 
+        <div className=" bg-cover bg-center absolute top-0 left-0 w-full h-full object-cover -z-10" 
         style={{animationDuration: '30s', animationTimingFunction: 'ease-in-out', animationIterationCount: 'infinite', animationDirection: 'alternate', 
         backgroundImage: `url('https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`}}></div>
         </div>
